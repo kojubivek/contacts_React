056ace05588d5f5fd72d6d495d5da64315298a4a
koju <kojubivek@gmail.com>
2022-11-02 10:28:06 +1100

    fixed module error

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,4 @@
-import Card from "../card/Card.component";
+import Card from "../card/card.component";
 
 const CardList = ({ contacts }) => (
   <div className="card-list">
@@ -7,6 +7,7 @@ const CardList = ({ contacts }) => (
         {contacts.map((contact) => {
           return (
             <div key={contact.id} className="col mb-3 justify-content-center">
+              {" "}
               <Card contact={contact} />
             </div>
           );
